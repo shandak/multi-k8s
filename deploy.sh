a728f9baae811d216073154ab2b9ebccbae69213
Volodymyr Shandak <vladimir@redweb.dk>
2019-07-04 22:52:31 +0300

    missed fix

--- deploy.sh
+++ deploy.sh
@@ -12,5 +12,5 @@ docker push shandak/multi-worker:$SHA
 
 kubectl apply -f k8s
 kubectl set image deployments/server-deployment server=shandak/multi-server:$SHA
-kubectl set image deployments/client-deployment server=shandak/multi-client:$SHA
-kubectl set image deployments/worker-deployment server=shandak/multi-worker:$SHA
+kubectl set image deployments/client-deployment client=shandak/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=shandak/multi-worker:$SHA
